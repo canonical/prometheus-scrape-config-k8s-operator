2dee6bea24246e9d781e4dc3e3692ef1756f99fb
Balbir Thomas <balbir.thomas@canonical.com>
2021-09-17 12:18:05 +0100

    A minimal scrape config charm implementation
    This commit implements a first draft of the scrape configuration
    charm. This charm relays scrape jobs and alert rules from
    scrape targets to Prometheus charms, reconfiguring the relayed
    scrape jobs in the process.

--- src/charm.py
+++ src/charm.py
@@ -7,8 +7,10 @@
 """Prometheus Scrape Configuration
 """
 
+import json
 import logging
 
+from charms.prometheus_k8s.v0.prometheus_scrape import MetricsEndpointConsumer
 from ops.charm import CharmBase
 from ops.main import main
 from ops.model import ActiveStatus
@@ -21,13 +23,58 @@ class PrometheusScrapeConfigCharm(CharmBase):
 
     def __init__(self, *args):
         super().__init__(*args)
-        self.framework.observe(self.on.config_changed, self._on_config_changed)
+
+        self._target_relation = "configurable-scrape-jobs"
+        self._prometheus_relation = "metrics-endpoint"
+
+        # manages relation with scrape targets
+        self._metrics_consumer = MetricsEndpointConsumer(self, self._target_relation)
+        self.framework.observe(
+            self._metrics_consumer.on.targets_changed, self._update_prometheus_jobs
+        )
+
+        # manages relation with Prometheus charm
+        self.framework.observe(
+            self.on[self._prometheus_relation].relation_joined, self._set_prometheus_jobs
+        )
+        self.framework.observe(
+            self.on[self._prometheus_relation].relation_changed, self._update_prometheus_jobs
+        )
+
+        # manages configuration changes for this charm
+        self.framework.observe(self.on.config_changed, self._update_prometheus_jobs)
         self.framework.observe(self.on.show_config_action, self._on_show_config_action)
 
-    def _on_config_changed(self, _):
-        """Update scrape configuration jobs.
+    def _set_prometheus_jobs(self, event):
+        """Set Prometheus scrape configuration for all targets.
         """
         self.unit.status = ActiveStatus()
+        if not self.unit.is_leader():
+            return
+
+        jobs = self._metrics_consumer.jobs()
+        for job in jobs:
+            job.update(self._config)
+        event.relation.data[self.app]["scrape_jobs"] = json.dumps(jobs)
+
+        groups = list(self._metrics_consumer.alerts().values())
+        event.relation.data[self.app]["alert_rules"] = json.dumps({"groups": groups})
+
+    def _update_prometheus_jobs(self, _):
+        """Update all scrape configuration jobs.
+        """
+        self.unit.status = ActiveStatus()
+        if not self.unit.is_leader():
+            return
+
+        for relation in self.model.relations[self._prometheus_relation]:
+            jobs = self._metrics_consumer.jobs()
+            for job in jobs:
+                job.update(self._config)
+            relation.data[self.app]["scrape_jobs"] = json.dumps(jobs)
+
+            groups = list(self._metrics_consumer.alerts().values())
+            relation.data[self.app]["alert_rules"] = json.dumps({"groups": groups})
 
     def _on_show_config_action(self, event):
         """Show current set of configured options.
@@ -36,7 +83,7 @@ class PrometheusScrapeConfigCharm(CharmBase):
 
     @property
     def _config(self):
-        return {key: value for key, value in self.model.config.items()}
+        return {key.replace('-', '_'): value for key, value in self.model.config.items()}
 
 
 if __name__ == "__main__":
