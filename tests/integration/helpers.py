1f12d950cdf33c7cfc5067687d07106a6f60a551
sed-i <82407168+sed-i@users.noreply.github.com>
2022-01-06 19:14:52 -0500

    add itests

---
+++ tests/integration/helpers.py
@@ -0,0 +1,16 @@
+# Copyright 2021 Canonical Ltd.
+# See LICENSE file for licensing details.
+
+import logging
+
+log = logging.getLogger(__name__)
+
+
+async def get_unit_address(ops_test, app_name: str, unit_num: int) -> str:
+    status = await ops_test.model.get_status()  # noqa: F821
+    return status["applications"][app_name]["units"][f"{app_name}/{unit_num}"]["address"]
+
+
+async def get_config_values(ops_test, app_name) -> dict:
+    config = await ops_test.model.applications[app_name].get_config()
+    return {key: config[key]["value"] for key in config if "value" in config[key]}
