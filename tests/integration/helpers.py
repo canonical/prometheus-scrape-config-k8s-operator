fc8eafae1d9cd3e7315e298d7e167ff4b2c8eb37
Dylan Stephano-Shachter <dylan.stephano-shachter@canonical.com>
2022-04-20 11:38:18 -0400

    Ensure alert rules can be passed from multiple workload charms (#13)
    * Ensure alert rules can be passed from multiple workload charms
    
    * Add Simon as a code owner
    
    * Return the proper value if there are no alert rules
    
    * Use += syntax
    
    Co-authored-by: Leon <82407168+sed-i@users.noreply.github.com>
    
    * Add some comments the the integration tests
    
    * add unit test
    
    * formatting and mypy
    
    * Add comments for easier reading
    
    Co-authored-by: Leon <82407168+sed-i@users.noreply.github.com>
    
    * make the test a bit more clear and add another for the trivial case
    
    * pin version of pytest-operator
    
    * deploy test dependencies in separate test
    
    * format with tox
    
    Co-authored-by: Leon <82407168+sed-i@users.noreply.github.com>

--- tests/integration/helpers.py
+++ tests/integration/helpers.py
@@ -1,14 +1,163 @@
+#!/usr/bin/env python3
 # Copyright 2021 Canonical Ltd.
 # See LICENSE file for licensing details.
 
-import logging
+from typing import List, Literal
 
-log = logging.getLogger(__name__)
+import aiohttp
+from pytest_operator.plugin import OpsTest
 
 
-async def get_unit_address(ops_test, app_name: str, unit_num: int) -> str:
-    status = await ops_test.model.get_status()  # noqa: F821
+class Prometheus:
+    """A class that represents a running instance of Prometheus."""
+
+    def __init__(self, host="localhost", port=9090):
+        """Utility to manage a Prometheus application.
+
+        Args:
+            host: Optional; host address of Prometheus application.
+            port: Optional; port on which Prometheus service is exposed.
+        """
+        self.base_url = f"http://{host}:{port}"
+
+    async def is_ready(self) -> bool:
+        """Send a GET request to check readiness.
+
+        Returns:
+          True if Prometheus is ready (returned 200 OK); False otherwise.
+        """
+        url = f"{self.base_url}/-/ready"
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url) as response:
+                return response.status == 200
+
+    async def config(self) -> str:
+        """Send a GET request to get Prometheus configuration.
+
+        Returns:
+          YAML config in string format or empty string
+        """
+        url = f"{self.base_url}/api/v1/status/config"
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url) as response:
+                result = await response.json()
+                return result["data"]["yaml"] if result["status"] == "success" else ""
+
+    async def rules(self, rules_type: Literal["alert", "record"] = None) -> list:
+        """Send a GET request to get Prometheus rules.
+
+        Args:
+          rules_type: the type of rules to fetch, or all types if not provided.
+
+        Returns:
+          Rule Groups list or empty list
+        """
+        url = f"{self.base_url}/api/v1/rules{'?type=' + rules_type if rules_type else ''}"
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url) as response:
+                result = await response.json()
+                # response looks like this:
+                # {"status":"success","data":{"groups":[]}
+                return result["data"]["groups"] if result["status"] == "success" else []
+
+    async def labels(self) -> List[str]:
+        """Send a GET request to get labels.
+
+        Returns:
+          List of labels
+        """
+        url = f"{self.base_url}/api/v1/labels"
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url) as response:
+                result = await response.json()
+                # response looks like this:
+                # {
+                #   "status": "success",
+                #   "data": [
+                #     "__name__",
+                #     "alertname",
+                #     "alertstate",
+                #     ...
+                #     "juju_application",
+                #     "juju_charm",
+                #     "juju_model",
+                #     "juju_model_uuid",
+                #     ...
+                #     "version"
+                #   ]
+                # }
+                return result["data"] if result["status"] == "success" else []
+
+    async def alerts(self) -> List[dict]:
+        """Send a GET request to get alerts.
+
+        Returns:
+          List of alerts
+        """
+        url = f"{self.base_url}/api/v1/alerts"
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url) as response:
+                result = await response.json()
+                # response looks like this:
+                #
+                # {
+                #   "status": "success",
+                #   "data": {
+                #     "alerts": [
+                #       {
+                #         "labels": {
+                #           "alertname": "AlwaysFiring",
+                #           "job": "non_existing_job",
+                #           "juju_application": "avalanche-k8s",
+                #           "juju_charm": "avalanche-k8s",
+                #           "juju_model": "remotewrite",
+                #           "juju_model_uuid": "5d2582f6-f8c9-4496-835b-675431d1fafe",
+                #           "severity": "High"
+                #         },
+                #         "annotations": {
+                #           "description": " of job non_existing_job is firing the dummy alarm.",
+                #           "summary": "Instance  dummy alarm (always firing)"
+                #         },
+                #         "state": "firing",
+                #         "activeAt": "2022-01-13T18:53:12.808550042Z",
+                #         "value": "1e+00"
+                #       }
+                #     ]
+                #   }
+                # }
+                return result["data"]["alerts"] if result["status"] == "success" else []
+
+
+async def unit_address(ops_test: OpsTest, app_name: str, unit_num: int) -> str:
+    """Find unit address for any application.
+
+    Args:
+        ops_test: pytest-operator plugin
+        app_name: string name of application
+        unit_num: integer number of a juju unit
+
+    Returns:
+        unit address as a string
+    """
+    status = await ops_test.model.get_status()
     return status["applications"][app_name]["units"][f"{app_name}/{unit_num}"]["address"]
+
+
+async def get_prometheus_rules(ops_test: OpsTest, app_name: str, unit_num: int) -> list:
+    """Fetch all Prometheus rules.
+
+    Args:
+        ops_test: pytest-operator plugin
+        app_name: string name of Prometheus application
+        unit_num: integer number of a Prometheus juju unit
+
+    Returns:
+        a list of rule groups.
+    """
+    host = await unit_address(ops_test, app_name, unit_num)
+    prometheus = Prometheus(host=host)
+    rules = await prometheus.rules()
+    return rules
 
 
 async def get_config_values(ops_test, app_name) -> dict:
---
+++ tests/integration/test_charm.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+# Copyright 2021 Canonical Ltd.
+# See LICENSE file for licensing details.
+
+import asyncio
+import logging
+
+import pytest
+from helpers import get_prometheus_rules
+
+logger = logging.getLogger(__name__)
+
+APP_NAME = "cos-config"
+PROM_NAME = "prometheus"
+SPRING_NAME = "spring-music"
+
+
+@pytest.mark.abort_on_fail
+async def test_dependencies(ops_test):
+    await asyncio.gather(
+        ops_test.model.deploy("ch:prometheus-k8s", application_name=PROM_NAME, channel="beta"),
+        ops_test.model.deploy("ch:spring-music", application_name=SPRING_NAME, channel="edge"),
+        ops_test.model.deploy("ch:spring-music", application_name="spring-music2", channel="edge"),
+    )
+    await ops_test.model.wait_for_idle(
+        status="active", apps=[PROM_NAME, SPRING_NAME, "spring-music2"]
+    )
+
+
+async def test_build_and_deploy(ops_test, charm_under_test):
+    await ops_test.model.deploy(charm_under_test, application_name=APP_NAME)
+    # The charm should be in blocked state if not related to anything.
+    await ops_test.model.wait_for_idle(status="blocked", apps=[APP_NAME])
+
+
+async def test_relate(ops_test):
+    await asyncio.gather(
+        ops_test.model.add_relation(PROM_NAME, APP_NAME),
+        ops_test.model.add_relation(APP_NAME, SPRING_NAME),
+    )
+    await ops_test.model.wait_for_idle(status="active")
+
+
+async def test_alert_rules_exist(ops_test):
+    rules = await get_prometheus_rules(ops_test=ops_test, app_name=PROM_NAME, unit_num=0)
+    assert len(rules) > 0, "No alert rules are present even though spring music is related"
+
+
+async def test_multiple_workloads_alert_rules(ops_test):
+    old_rules = await get_prometheus_rules(ops_test=ops_test, app_name=PROM_NAME, unit_num=0)
+    await ops_test.model.add_relation(APP_NAME, "spring-music2")
+    await ops_test.model.wait_for_idle(status="active")
+    new_rules = await get_prometheus_rules(ops_test=ops_test, app_name=PROM_NAME, unit_num=0)
+    assert len(new_rules) > len(old_rules), "Additional workload instance did not add alert rules"
