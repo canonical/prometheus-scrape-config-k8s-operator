0525afcbb6dfd20e4d43a88632fa2b61a57a263e
Balbir Thomas <balbir.thomas@canonical.com>
2022-07-21 13:11:28 +0100

    Linting fixes for integration tests
    This commit only adds linting fixes for integration tests without
    any functional changes.

--- tests/integration/test_charm.py
+++ tests/integration/test_charm.py
@@ -22,9 +22,7 @@ async def test_dependencies(ops_test):
         ops_test.model.deploy("ch:zinc-k8s", application_name=ZINC_NAME, channel="edge"),
         ops_test.model.deploy("ch:zinc-k8s", application_name="zinc2", channel="edge"),
     )
-    await ops_test.model.wait_for_idle(
-        status="active", apps=[PROM_NAME, ZINC_NAME, "zinc2"]
-    )
+    await ops_test.model.wait_for_idle(status="active", apps=[PROM_NAME, ZINC_NAME, "zinc2"])
 
 
 async def test_build_and_deploy(ops_test, charm_under_test):
@@ -35,8 +33,12 @@ async def test_build_and_deploy(ops_test, charm_under_test):
 
 async def test_relate(ops_test):
     await asyncio.gather(
-        ops_test.model.add_relation(f"{PROM_NAME}:metrics-endpoint", f"{APP_NAME}:metrics-endpoint"),
-        ops_test.model.add_relation(f"{APP_NAME}:configurable-scrape-jobs", f"{ZINC_NAME}:metrics-endpoint"),
+        ops_test.model.add_relation(
+            f"{PROM_NAME}:metrics-endpoint", f"{APP_NAME}:metrics-endpoint"
+        ),
+        ops_test.model.add_relation(
+            f"{APP_NAME}:configurable-scrape-jobs", f"{ZINC_NAME}:metrics-endpoint"
+        ),
     )
     await ops_test.model.wait_for_idle(status="active")
 
@@ -53,11 +55,10 @@ async def test_multiple_workloads_alert_rules(ops_test):
     new_rules = await get_prometheus_rules(ops_test=ops_test, app_name=PROM_NAME, unit_num=0)
     assert len(new_rules) > len(old_rules), "Additional workload instance did not add alert rules"
 
+
 async def test_non_leader_units_set_waiting_status(ops_test):
     await ops_test.model.applications[APP_NAME].scale(scale=2)
-    await ops_test.model.block_until(
-        lambda: len(ops_test.model.applications[APP_NAME].units) == 2
-    )
+    await ops_test.model.block_until(lambda: len(ops_test.model.applications[APP_NAME].units) == 2)
     await ops_test.model.wait_for_idle()
     statuses = []
     for unit in ops_test.model.applications[APP_NAME].units:
