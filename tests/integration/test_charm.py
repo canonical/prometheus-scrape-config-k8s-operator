8037dd10e106f6ff9f982a00a300b4aedc7525dd
Balbir Thomas <balbir.thomas@canonical.com>
2022-07-21 11:21:55 +0100

    Added integration test to check non leader status
    This commit adds an integration test to check that non leader
    units set waiting status.
    
    closes #9

--- tests/integration/test_charm.py
+++ tests/integration/test_charm.py
@@ -52,3 +52,16 @@ async def test_multiple_workloads_alert_rules(ops_test):
     await ops_test.model.wait_for_idle(status="active")
     new_rules = await get_prometheus_rules(ops_test=ops_test, app_name=PROM_NAME, unit_num=0)
     assert len(new_rules) > len(old_rules), "Additional workload instance did not add alert rules"
+
+async def test_non_leader_units_set_waiting_status(ops_test):
+    await ops_test.model.applications[APP_NAME].scale(scale=2)
+    await ops_test.model.block_until(
+        lambda: len(ops_test.model.applications[APP_NAME].units) == 2
+    )
+    await ops_test.model.wait_for_idle()
+    statuses = []
+    for unit in ops_test.model.applications[APP_NAME].units:
+        statuses.append(unit.workload_status)
+    assert len(statuses) == 2
+    assert "active" in statuses
+    assert "waiting" in statuses
