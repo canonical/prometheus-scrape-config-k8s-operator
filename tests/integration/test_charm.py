d66ae64574a9d8e89bdf19c5b476f262aaeacd0c
Balbir Thomas <balbir.thomas@canonical.com>
2022-07-22 14:23:21 +0100

    Fix integration tests and add test for non leader unit status (#16)
    * Set asyncio mode to auto
    
    This commit ensures asyncio mode set to auto so that pytest fixtures
    are run.
    
    * Update pytest operator version
    
    This commit updates to the most recent version of pytest operator.
    
    * Use zinc instead of spring music
    
    This commit migrates the integration tests to use the Zinc k8s
    charm instead of the Spring music charm.
    
    * Added integration test to check non leader status
    
    This commit adds an integration test to check that non leader
    units set waiting status.
    
    closes #9
    
    * Linting fixes for integration tests
    
    This commit only adds linting fixes for integration tests without
    any functional changes.
    
    * Add trust option for deploying Prometheus
    
    Recent changes require adding the trust option while deploying
    the Prometheus charm.
    
    * Switched integration test to use edge Prometheus
    
    This commit switches the integration test to use Prometheus
    from edge.
    
    * Updated to latest Prometheus library
    
    This commit updates the charm to use the latest Prometheus
    library.
    
    * Use released version of ops
    
    In preparation of a candidate release this commit ensures that
    Operator Framework used is a released version rather than github.
    
    * Fix static analysis failure due to mypy missing variable
    
    This commit fixes a failure in static analysis caused by
    a missing variable in recent releases of mypy.
    
    * Remove unused libraries from mypy overrides
    
    This commit removes libraries mentioned in mypy overrieds
    but are actually not used by this charm.
    
    * Fix static analysis target deps
    
    This commit fixes the dependencies for static analysis to be the
    minimal required.
    
    * Review comment fix - remove app names from wait for idle
    
    This commit responds to a review comment by removing the names
    of the apps in wait for idle method.

--- tests/integration/test_charm.py
+++ tests/integration/test_charm.py
@@ -12,19 +12,19 @@ logger = logging.getLogger(__name__)
 
 APP_NAME = "cos-config"
 PROM_NAME = "prometheus"
-SPRING_NAME = "spring-music"
+ZINC_NAME = "zinc"
 
 
 @pytest.mark.abort_on_fail
 async def test_dependencies(ops_test):
     await asyncio.gather(
-        ops_test.model.deploy("ch:prometheus-k8s", application_name=PROM_NAME, channel="beta"),
-        ops_test.model.deploy("ch:spring-music", application_name=SPRING_NAME, channel="edge"),
-        ops_test.model.deploy("ch:spring-music", application_name="spring-music2", channel="edge"),
+        ops_test.model.deploy(
+            "ch:prometheus-k8s", application_name=PROM_NAME, channel="edge", trust=True
+        ),
+        ops_test.model.deploy("ch:zinc-k8s", application_name=ZINC_NAME, channel="edge"),
+        ops_test.model.deploy("ch:zinc-k8s", application_name="zinc2", channel="edge"),
     )
-    await ops_test.model.wait_for_idle(
-        status="active", apps=[PROM_NAME, SPRING_NAME, "spring-music2"]
-    )
+    await ops_test.model.wait_for_idle(status="active")
 
 
 async def test_build_and_deploy(ops_test, charm_under_test):
@@ -35,20 +35,36 @@ async def test_build_and_deploy(ops_test, charm_under_test):
 
 async def test_relate(ops_test):
     await asyncio.gather(
-        ops_test.model.add_relation(PROM_NAME, APP_NAME),
-        ops_test.model.add_relation(APP_NAME, SPRING_NAME),
+        ops_test.model.add_relation(
+            f"{PROM_NAME}:metrics-endpoint", f"{APP_NAME}:metrics-endpoint"
+        ),
+        ops_test.model.add_relation(
+            f"{APP_NAME}:configurable-scrape-jobs", f"{ZINC_NAME}:metrics-endpoint"
+        ),
     )
     await ops_test.model.wait_for_idle(status="active")
 
 
 async def test_alert_rules_exist(ops_test):
     rules = await get_prometheus_rules(ops_test=ops_test, app_name=PROM_NAME, unit_num=0)
-    assert len(rules) > 0, "No alert rules are present even though spring music is related"
+    assert len(rules) > 0, "No alert rules are present even though zinc is related"
 
 
 async def test_multiple_workloads_alert_rules(ops_test):
     old_rules = await get_prometheus_rules(ops_test=ops_test, app_name=PROM_NAME, unit_num=0)
-    await ops_test.model.add_relation(APP_NAME, "spring-music2")
+    await ops_test.model.add_relation(APP_NAME, "zinc2")
     await ops_test.model.wait_for_idle(status="active")
     new_rules = await get_prometheus_rules(ops_test=ops_test, app_name=PROM_NAME, unit_num=0)
     assert len(new_rules) > len(old_rules), "Additional workload instance did not add alert rules"
+
+
+async def test_non_leader_units_set_waiting_status(ops_test):
+    await ops_test.model.applications[APP_NAME].scale(scale=2)
+    await ops_test.model.block_until(lambda: len(ops_test.model.applications[APP_NAME].units) == 2)
+    await ops_test.model.wait_for_idle()
+    statuses = []
+    for unit in ops_test.model.applications[APP_NAME].units:
+        statuses.append(unit.workload_status)
+    assert len(statuses) == 2
+    assert "active" in statuses
+    assert "waiting" in statuses
